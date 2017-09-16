f612f9a382fff8c428a908293862f7eb28f1b425
denghang <denghang1989@163.com>
2017-09-16 20:47:51 +0800

    新建工程

---
+++ fix_phone/app/src/main/java/dhcc/cn/com/fix_phone/MainActivity.java
@@ -0,0 +1,13 @@
+package dhcc.cn.com.fix_phone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
